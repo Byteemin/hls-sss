cc1641a1a4717db1b14ea297f56aa89fa2821128
Vasilev Vladimir <79542050+Byteemin@users.noreply.github.com>
2023-07-06 18:54:54 +0300

    добавил остановку и старт на клик и пробел

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// alert('hi!');
+var video = document.querySelector('video');
+var play_stope_bool = false;
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space' & play_stope_bool) {
+    video.play();
+    play_stope_bool = false;
+  }
+  else {
+    video.pause();
+    play_stope_bool = true;  
+  }
+});
+document.addEventListener('click', function() {
+  if (play_stope_bool) {
+    video.play();
+    play_stope_bool = false;
+  }
+  else {
+    video.pause();
+    play_stope_bool = true;  
+  }
+});
+
