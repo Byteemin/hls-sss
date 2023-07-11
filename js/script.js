93d1e60286db047b6a17d6c5623b5a3a9a00a2da
Vasilev Vladimir <79542050+Byteemin@users.noreply.github.com>
2023-07-11 17:31:01 +0300

    переписал код скрытие/появление панели скучковал

--- js/script.js
+++ js/script.js
@@ -1,65 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var seconds_for_rewind = 10;
   var video = document.getElementById('video');
   var hiddenPanel = document.querySelector('.controls');
   var timeout;
-  var isClickEvent = false; // Флаг для определения события клика
+  var seconds_for_rewind = 10;                                                        // Секунды для перемотки
+  var isClickEvent = false;                                                          // Флаг для определения события клика
+  var timeline = document.querySelector('.player-sidebar__time-line');
+  var progress = document.querySelector('.player-sidebar__time-line--filling');
+  var timeLeft  = document.querySelector('.controls__vidoe-time');
 
+// Поддержка hls
   if (Hls.isSupported()) {
     var video = document.getElementById('video');
     var config = {
       autoStartLoad: true,
     };
-    
     var hls = new Hls(config);
-
-    hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-      console.log('video and hls.js are now bound together !');
-    });
-    hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-      console.log(
-        'manifest loaded, found ' + data.levels.length + ' quality level'
-      );
-    });
     hls.loadSource('https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8');
     // bind them together
     hls.attachMedia(video);
   }
 
-
- var mouseReaction = true;
-
-
-  document.addEventListener('mousemove', appearanceControls);   // Обработчик события движения мыши
-
-// Появление/скрытие панели
-  function appearanceControls(event) {
-    if (event.type ==='mousemove' && mouseReaction) {
-      hiddenPanel.style.display = 'flex';
-      resetTimer();
-    }
-    else {
-      if (video.paused) {
-        hiddenPanel.style.display = 'none';
-        mouseReaction = false;
-      }
-      else {
-        hiddenPanel.style.display = 'flex';
-        mouseReaction = false;
-      }
-    }
-  }
-
-// Таймер для сброса значения mouseMoved
-  function resetTimer() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      hiddenPanel.style.display = 'none';
-    }, 2000); // Время задержки перед сокрытием панели (2 секунды)
-  }
-
-  
+// Обработчик события движения мыши
+  document.addEventListener('mousemove', appearanceControls);
+// Обработчик события окончания клика на видео
+  video.addEventListener('mouseup', function() {
+    isClickEvent = false;
+  });
 
 
 
@@ -67,37 +34,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Обработчик события нажатия клавиши Пробел
   document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space' && !isClickEvent) {
-      appearanceControls(event);       
+    if (event.code === 'Space' && !isClickEvent) {     
       togglePlayPause();
     }
   });
-  
-// Обработчик события клика на видео
-  video.addEventListener('click', function(event) {
-    isClickEvent = true;
-    appearanceControls(event);
-    togglePlayPause();
-  });
-  
-// Обработчик события окончания клика на видео
-  video.addEventListener('mouseup', function() {
-    isClickEvent = false;
-  });
-
-// Функция для переключения паузы и старта видео
-function togglePlayPause() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
+// Обработчик события нажатия на клавишу
+document.addEventListener('keydown', function(event) {
+  switch (event.key) {
+    case 'ArrowLeft':
+      rewindVideo(-10); // перемотка назад на 10 секунд
+      break;
+    case 'ArrowRight':
+      rewindVideo(10); // перемотка вперед на 10 секунд
+      break;
   }
-}
+});
+// Включение полноэкранного режима по нажатию на клавишу "Enter"
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      video.requestFullscreen();
+      video.controls = false;
+    }
+  });
 
-  // Полоса видео и сколько осталось в минутах
-  var timeline = document.querySelector('.player-sidebar__time-line');
-  var progress = document.querySelector('.player-sidebar__time-line--filling');
-  var timeLeft  = document.querySelector('.controls__vidoe-time');
+
+
+// Обработчик события клика на видео
+video.addEventListener('click', function(event) {
+  isClickEvent = true;
+  togglePlayPause();
+});
 
 // Клик по тайм лайн
   timeline.addEventListener('click', function(event) {
@@ -109,6 +75,8 @@ function togglePlayPause() {
   });
   
 
+
+  
 // Полоса видео
   video.addEventListener('timeupdate', function() {
     var currentTime = video.currentTime;
@@ -126,29 +94,38 @@ function togglePlayPause() {
     timeLeft.textContent = formattedTime;
   });
 
-// Обработчик события нажатия на клавишу
-  document.addEventListener('keydown', function(event) {
-    switch (event.key) {
-      case 'ArrowLeft':
-        rewindVideo(-10); // перемотка назад на 10 секунд
-        break;
-      case 'ArrowRight':
-        rewindVideo(10); // перемотка вперед на 10 секунд
-        break;
-    }
-  });
 
-  // Функция для перемотки видео
+// Таймер для сброса панели
+function resetTimer() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    hiddenPanel.style.display = 'none';
+  }, 2000); // Время задержки перед сокрытием панели (2 секунды)
+}
+
+// Функция для переключения паузы/старта видео
+  function togglePlayPause() {
+    if (video.paused) {
+      video.play();
+      appearanceControls();
+    } else {
+      video.pause();
+      appearanceControls();
+    }
+  }
+  // Функция появление/скрытие панели
+  function appearanceControls() {
+    if (!video.paused) { // Если видео не на паузе показ панели и сброс через 2 секунды
+      hiddenPanel.style.display = 'flex';
+      resetTimer();
+    }
+    else {
+      hiddenPanel.style.display = 'flex';
+    }
+  }
+
+// Функция для перемотки видео
   function rewindVideo(seconds_for_rewind) {
     video.currentTime += seconds_for_rewind;
   }
-
-
-  // Включение полноэкранного режима по нажатию на клавишу "Enter"
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    video.requestFullscreen();
-    video.controls = false;
-  }
-});
 });
