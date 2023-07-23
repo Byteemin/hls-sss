6355b945b9d3e4d7fea8f1b01f951d04d708a75b
Byteemin <hachiiiroketo@gmail.com>
2023-07-23 19:59:29 +0300

    работа с апи

---
+++ js/assistance.js
@@ -0,0 +1,23 @@
+const apiKey = 'https://pl.iptv2021.com/api/v4/playlist';
+// Занесем тестовый URL в переменну
+
+
+// Феч запрос к серверу
+function sendRequest(method, url, body = null) {
+  const headers = {
+    'User-Agent': '{"platform":"android","app":"limehd.ru.ctv.nsk","version_name":"2.8.3","version_code":"286","sdk":"29","name":"PHONE","device_id":"43592e017890be21"}'
+  };
+  return fetch(url, {
+    method: method,
+    headers: headers
+  }).then( response => {
+    return response.json()
+  })
+}
+
+
+
+
+
+
+export { apiKey, sendRequest };
